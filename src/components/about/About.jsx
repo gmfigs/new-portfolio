9eb6a7cb9bff589b83bb72863ee295fa6dcb92dd
goncalo_mf <gm.figs@gmail.com>
2024-07-22 18:07:10 +0100

    download the correct CV

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpeg";
-import CV from "../../assets/ux_cv.pdf";
+import CV from "../../assets/GF-Resume-A4.pdf";
 import Info from "./Info";
 
 const About = () => {
